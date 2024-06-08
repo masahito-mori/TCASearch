e032bdf547ad868c2cd676c2e81b7a434caba549
masahito-mori <mmori.0426@gmail.com>
2024-06-08 21:40:34 +0900

    💚[天気取得]テストコードを記述
    【対応内容】
    ・天気取得処理のテストコードを記述した。
    
    【動作確認】
    ・天気取得処理のテストが成功すること。

--- TCASampleSearchTests/TCASampleSearchTests.swift
+++ TCASampleSearchTests/TCASampleSearchTests.swift
@@ -52,4 +52,159 @@ final class TCASampleSearchTests: XCTestCase {
         await store.send(.searchQueryChangeDebounced)
         await store.receive(\.searchResponse.failure)
     }
+    
+    @MainActor
+    func testClearQueryCancelsInFlightSearchRequest() async {
+        let store = TestStore(initialState: SearchFeature.State()) {
+            SearchFeature()
+        } withDependencies: {
+            $0.weatherAPIClient.search = { @Sendable _ in .mock }
+        }
+        
+        let searchQueryChanged = await store.send(.searchQueryChanged("S")) {
+            $0.searchQuery = "S"
+        }
+        await searchQueryChanged.cancel()
+        await store.send(.searchQueryChanged("")) {
+            $0.searchQuery = ""
+        }
+    }
+    
+    @MainActor
+    func testTapOnLocation() async {
+        let specialResult = GeocodingSearch.Result(
+            country: "Special Country",
+            latitude: 0,
+            longitude: 0,
+            id: 42,
+            name: "Special Place"
+        )
+        
+        var results = GeocodingSearch.mock.results
+        results.append(specialResult)
+        
+        let store = TestStore(initialState: SearchFeature.State()) {
+            SearchFeature()
+        } withDependencies: {
+            $0.weatherAPIClient.forecast = { @Sendable _ in .mock }
+        }
+        
+        await store.send(.searchResultTapped(specialResult)) {
+            $0.resultForecastRequestInFlight = specialResult
+        }
+        
+        await store.receive(\.forecastResponse) {
+            $0.resultForecastRequestInFlight = nil
+            $0.weather = SearchFeature.State.Weather(
+                id: 42,
+                days: [
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 0),
+                        temperatureMax: 90,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 70,
+                        temperatureMinUnit: "°F"
+                    ),
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 86_400),
+                        temperatureMax: 70,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 50,
+                        temperatureMinUnit: "°F"
+                    ),
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 172_800),
+                        temperatureMax: 100,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 80,
+                        temperatureMinUnit: "°F"
+                    ),
+                ]
+            )
+        }
+    }
+    
+    @MainActor
+    func testTapOnLocationCancelInFlightRequest() async {
+        let specialResult = GeocodingSearch.Result(
+            country: "Special Country",
+            latitude: 0,
+            longitude: 0,
+            id: 42,
+            name: "Special Place"
+        )
+        
+        var results = GeocodingSearch.mock.results
+        results.append(specialResult)
+        
+        let clock = TestClock()
+        
+        let store = TestStore(initialState: SearchFeature.State(results: results)) {
+            SearchFeature()
+        } withDependencies: {
+            $0.weatherAPIClient.forecast = { @Sendable _ in
+                try await clock.sleep(for: .seconds(0))
+                return .mock
+            }
+        }
+        
+        await store.send(.searchResultTapped(results.first!)) {
+            $0.resultForecastRequestInFlight = results.first!
+        }
+        await store.send(.searchResultTapped(specialResult)) {
+            $0.resultForecastRequestInFlight = specialResult
+        }
+        
+        await clock.advance()
+        await store.receive(\.forecastResponse) {
+            $0.resultForecastRequestInFlight = nil
+            $0.weather = SearchFeature.State.Weather(
+                id: 42,
+                days: [
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 0),
+                        temperatureMax: 90,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 70,
+                        temperatureMinUnit: "°F"
+                    ),
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 86_400),
+                        temperatureMax: 70,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 50,
+                        temperatureMinUnit: "°F"
+                    ),
+                    SearchFeature.State.Weather.Day(
+                        date: Date(timeIntervalSince1970: 172_800),
+                        temperatureMax: 100,
+                        temperatureMaxUnit: "°F",
+                        temperatureMin: 80,
+                        temperatureMinUnit: "°F"
+                    ),
+                ]
+            )
+        }
+    }
+    
+    @MainActor
+    func testTapOnLocationFailure() async {
+        let results = GeocodingSearch.mock.results
+        
+        let store = TestStore(initialState: SearchFeature.State(results: results)) {
+            SearchFeature()
+        } withDependencies: {
+            $0.weatherAPIClient.forecast = { @Sendable _ in
+                struct SomethingWentWrong: Error {}
+                throw SomethingWentWrong()
+            }
+        }
+        
+        await store.send(.searchResultTapped(results.first!)) {
+            $0.resultForecastRequestInFlight = results.first!
+        }
+        await store.receive(\.forecastResponse) {
+            $0.resultForecastRequestInFlight = nil
+        }
+    }
 }
